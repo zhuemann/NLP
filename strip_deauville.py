a64186a8313163f5a27ae1b14894f1409b29a99b
zhuemann <56049645+zhuemann@users.noreply.github.com>
2021-08-05 22:13:04 -0500

    Counts number of ds scores

--- strip_deauville.py
+++ strip_deauville.py
@@ -1,10 +1,10 @@
-
 from nlp_utils import *
 import os
 import csv
 from sklearn.model_selection import train_test_split
 from nltk.tokenize import word_tokenize, sent_tokenize
 from classification_model_doc2vec import clean_up_text
+
 
 # Need to
 # 1. Remove dates
@@ -15,10 +15,13 @@ from classification_model_doc2vec import clean_up_text
 
 def highest_deauville(text):
     highest = ''
-    for i in range(1,6):
+    scores_found = 0
+    for i in range(1, 6):
         if text.find('deauvil_score_' + str(i)) > -1:
             highest = 'deauvil_score_' + str(i)
-    return highest
+            scores_found += 1
+    return highest, scores_found
+
 
 def remove_deauville(text_filt):
     replacement_word = 'score'
@@ -30,21 +33,18 @@ def remove_deauville(text_filt):
     return text_filt
 
 
-
-
-
 def run_deauville_stripping():
     # Loads the impressions/fidings, finds which ones have deauville scoers, does some basic text processing, and
     # saves the text and DS in a new file. Note the options in the first few lines
 
-    #comment out the onese you don't want
+    # comment out the onese you don't want
     options = ['', '']
-    options[0] = 'combo'  #combine findings and impression
+    options[0] = 'combo'  # combine findings and impression
     # options[0] = 'either' #take impression only, or finding in absece of impression
-    options[1] = 'more_synonyms' #replace more synonyms beyond Deauville
+    options[1] = 'more_synonyms'  # replace more synonyms beyond Deauville
     # options[1] = 'no_synonyms' #don't replace more synonyms
 
-    #direct = '/home/tjb129/r-fcb-isilon/research/Bradshaw/Lymphoma_UW_Retrospective/Reports'
+    # direct = '/home/tjb129/r-fcb-isilon/research/Bradshaw/Lymphoma_UW_Retrospective/Reports'
     direct = 'Z:\Lymphoma_UW_Retrospective\Reports'
     indications_file = 'indications.xlsx'
 
@@ -60,11 +60,10 @@ def run_deauville_stripping():
 
     save_file = save_file + '.csv'
 
-
     synonyms_file = os.path.join(direct, 'deauville_replacements.xlsx')
     synonyms_sheet = 'ngram'
 
-    more_synonyms_file =  os.path.join(direct, 'synonyms_in_reports_v2.xlsx')
+    more_synonyms_file = os.path.join(direct, 'synonyms_in_reports_v2.xlsx')
     more_synonyms_sheet = 'synonyms'
 
     # read in full data
@@ -80,11 +79,10 @@ def run_deauville_stripping():
         more_words_to_replace = more_syns['Word']
         more_replacements = more_syns['Replacement']
 
-
-
     findings = df['impression']
     filtered_findings = []
     deauville_scores = []
+    hedging = []
     subj_id = []
 
     # !!!!!!!!!!!!!!!!!!!!!!!!HOW TO CORRECT SPELLING ERRORS?    #!!!!!!!!!!!!!!!!!!!!!!!!
@@ -97,6 +95,7 @@ def run_deauville_stripping():
         if type(text) is not str:
             filtered_findings.append('nan')
             deauville_scores.append('nan')
+            hedging.append(0)
 
         else:
             # remove punctuation
@@ -114,11 +113,12 @@ def run_deauville_stripping():
             # # contractions
             text_filt = remove_contractions_and_hyphens(text_filt)
             # get the highest deauville score
-            ds = highest_deauville(text_filt)
+            ds, scores_found = highest_deauville(text_filt)
             # remove the deuville scores
             text_filt = remove_deauville(text_filt)
             filtered_findings.append(text_filt)
-            deauville_scores.append(ds.replace('deauvil_score_', '')) #store just the number
+            deauville_scores.append(ds.replace('deauvil_score_', ''))  # store just the number
+            hedging.append(scores_found)
 
     # remove low frequency words
     # filtered_findings = remove_low_frequency_words_and_repeated_words(filtered_findings, freq=10)
@@ -127,13 +127,13 @@ def run_deauville_stripping():
     df['impression_processed'] = filtered_findings
     df['deauville'] = deauville_scores
     df = df.set_index('accession')
-    #df.to_csv(os.path.join(direct, save_file))
+    df['num_scores'] = hedging
+    # df.to_csv(os.path.join(direct, save_file))
     df.to_csv(os.path.join("Z:\\Zach_Analysis\\text_data", save_file))
 
 
 def run_split_reports_according_to_ds(options='binary'):
-    #options can be 'binary' or '5-level'
-
+    # options can be 'binary' or '5-level'
 
     direct = '/home/tjb129/r-fcb-isilon/research/Bradshaw/Lymphoma_UW_Retrospective/Reports'
     read_file = 'findings_and_impressions_wo_ds_more_syn.csv'
@@ -141,9 +141,9 @@ def run_split_reports_according_to_ds(options='binary'):
     df = pd.read_csv(os.path.join(direct, read_file))
 
     if options == 'binary':
-        cut_points = [[1,2,3],[4,5]]
+        cut_points = [[1, 2, 3], [4, 5]]
     elif options == '5-level':
-        cut_points = [[1],[2],[3],[4],[5]]
+        cut_points = [[1], [2], [3], [4], [5]]
     else:
         print('wrong options')
         exit()
@@ -155,22 +155,23 @@ def run_split_reports_according_to_ds(options='binary'):
             if row["deauville"] in cut:
                 category_i.append(row["impression_processed"])
                 id.append(row['accession'])
-        df_save = pd.DataFrame(list(zip(id, category_i)), columns = ['id','text'])
+        df_save = pd.DataFrame(list(zip(id, category_i)), columns=['id', 'text'])
         df_save = df_save.set_index('id')
-        save_name = 'ds' + str(cut).replace('[','').replace(']','').replace(',','').replace(' ', '') + '_' + read_file
+        save_name = 'ds' + str(cut).replace('[', '').replace(']', '').replace(',', '').replace(' ',
+                                                                                               '') + '_' + read_file
         df_save = df_save.sort_values('id')
-        df_save.to_csv(os.path.join(direct,save_name))
+        df_save.to_csv(os.path.join(direct, save_name))
 
 
-
-def interpretability_replace_key_sentences_in_test_dataset(test_fract = 0.2,
-                                        test_seed = 33,
-                                        num_reports_to_alter = 20,
-                                        # report_processed = 'doc2vec_ds_5_classes_processed.csv', #processed, cleaned text, containing all cases and labels, optional
-                                        report_files = ['ds123_findings_and_impressions_wo_ds_more_syn.csv', 'ds45_findings_and_impressions_wo_ds_more_syn.csv' ],
-                                        decoy_sentences = 'replacement_sentences_positive_and_negative.csv'
- ):
-
+def interpretability_replace_key_sentences_in_test_dataset(test_fract=0.2,
+                                                           test_seed=33,
+                                                           num_reports_to_alter=20,
+                                                           # report_processed = 'doc2vec_ds_5_classes_processed.csv', #processed, cleaned text, containing all cases and labels, optional
+                                                           report_files=[
+                                                               'ds123_findings_and_impressions_wo_ds_more_syn.csv',
+                                                               'ds45_findings_and_impressions_wo_ds_more_syn.csv'],
+                                                           decoy_sentences='replacement_sentences_positive_and_negative.csv'
+                                                           ):
     # report_direct = '/home/tjb129/r-fcb-isilon/research/Bradshaw/Lymphoma_UW_Retrospective/Reports'
     # df = pd.read_csv(os.path.join(report_direct,report_processed))
     # df = df.sort_values('id')
@@ -179,17 +180,17 @@ def interpretability_replace_key_sentences_in_test_dataset(test_fract = 0.2,
     # else:
     #     replacement_sentences = pd.read_csv(os.path.join(report_direct, 'replacement_sentences_positive_and_negative.csv'))
 
-      #load data first
+    # load data first
 
     report_direct = '/home/tjb129/r-fcb-isilon/research/Bradshaw/Lymphoma_UW_Retrospective/Reports'
-    df = pd.DataFrame(columns=['id','text'])
-    for i,file in enumerate(report_files):
+    df = pd.DataFrame(columns=['id', 'text'])
+    for i, file in enumerate(report_files):
         df0 = pd.read_csv(os.path.join(report_direct, file))
-        if len(report_files) > 2: #multi-class, one-hot
+        if len(report_files) > 2:  # multi-class, one-hot
             label_i = [0] * len(report_files)
             label_i[i] = 1
             df0['labels'] = [label_i] * len(df0)
-        else: #binary
+        else:  # binary
             df0['labels'] = i
         # df0 = df0.set_index('id')
         df = pd.concat([df, df0], axis=0, join='outer')
@@ -197,17 +198,16 @@ def interpretability_replace_key_sentences_in_test_dataset(test_fract = 0.2,
     df = df.set_index('id')
     df = df.sort_values('id')
 
-
-    #randomly split into train, test, then validations
+    # randomly split into train, test, then validations
     _, df_test = train_test_split(df, test_size=test_fract, random_state=test_seed)
-    df_test_replaced = pd.DataFrame(columns=['id', 'labels', 'text_processed'])  #new dataframe with replacements
+    df_test_replaced = pd.DataFrame(columns=['id', 'labels', 'text_processed'])  # new dataframe with replacements
     df_test = df_test.sort_values('id')
     replacement_sentences = pd.read_csv(os.path.join(report_direct, decoy_sentences))
 
-    replacement_info = []  #store info on which sentences to replace
+    replacement_info = []  # store info on which sentences to replace
     for i in range(num_reports_to_alter):
         sentences = sent_tokenize(df_test["text"].iloc[i])
-        print('DS = ' +  str(df_test["labels"].iloc[i]) + ' , ID = ' + str(df_test.index.values[i]))
+        print('DS = ' + str(df_test["labels"].iloc[i]) + ' , ID = ' + str(df_test.index.values[i]))
         new_report = ''
         for j, sent in enumerate(sentences):
             print(str(j) + ':  ' + sent)
@@ -217,29 +217,26 @@ def interpretability_replace_key_sentences_in_test_dataset(test_fract = 0.2,
         # pos_or_neg = input('Replace with disease-positive sentences (1) or disease-negative (0): ')
         pos_or_neg = 0 if df_test["labels"].iloc[i] > 2 else 1
         replacement_info.append([i, df_test.index.values[i], to_replace])
-        head ='positive_sentence' if pos_or_neg == 1 else 'negative_sentence'
+        head = 'positive_sentence' if pos_or_neg == 1 else 'negative_sentence'
         for j, sent in enumerate(sentences):
             if j in to_replace:
                 sent = list(replacement_sentences.sample()[head])
                 sent = sent[0]
             new_report = new_report + ' ' + sent
-        row_to_add = pd.Series({'labels': df_test["labels"].iloc[i], 'text': new_report , 'id':df_test.index.values[i]})
+        row_to_add = pd.Series({'labels': df_test["labels"].iloc[i], 'text': new_report, 'id': df_test.index.values[i]})
         df_test_replaced = df_test_replaced.append(row_to_add, ignore_index=True)
-
-
 
         with open('log_file.txt', 'a+') as f:
             f.write(str(i) + "\n")
             f.write(df_test.index.values[i] + '\n')
-            f.write(str(to_replace)+ "\n")
-
+            f.write(str(to_replace) + "\n")
 
     df_test_replaced.to_csv(os.path.join(report_direct, 'DECOYS_' + 'bert_' + str(len(report_files)) + '_classes.csv'))
-    #now for doc2vec, need special language processing
+    # now for doc2vec, need special language processing
     df_test_replaced_doc2vec = df_test_replaced.copy()
     df_test_replaced_doc2vec = clean_up_text(df_test_replaced_doc2vec, remove_useless_sentences=0)
     df_test_replaced_doc2vec = df_test_replaced_doc2vec.drop(['text'], axis=1)
-    df_test_replaced_doc2vec = df_test_replaced_doc2vec.rename(columns={'text_processed':'text'})
+    df_test_replaced_doc2vec = df_test_replaced_doc2vec.rename(columns={'text_processed': 'text'})
     df_test_replaced_doc2vec.columns
-    df_test_replaced_doc2vec.to_csv(os.path.join(report_direct, 'DECOYS_' + 'doc2vec_' + str(len(report_files)) + '_classes.csv'))
-
+    df_test_replaced_doc2vec.to_csv(
+        os.path.join(report_direct, 'DECOYS_' + 'doc2vec_' + str(len(report_files)) + '_classes.csv'))
